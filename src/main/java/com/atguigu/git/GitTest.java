e29662353ce9aec002d0b2f3b8fc3936dff27756
ÈÎÑǲ¨ <renyb50381@hundsun.com>
2023-06-07 12:47:28 +0800

    3
    (cherry picked from commit c8151ec629f1d7e0a954cbb296397fc8fa5a537d)

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,10 +10,5 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("pull test");
         System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("testtest");
-        System.out.println("pull test");
-        System.out.println("pull test");
-        System.out.println("pull test");
     }
 }
